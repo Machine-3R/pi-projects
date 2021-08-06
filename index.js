e9e28450ca687805e36bb3f9f9c065a6f247368b
Machine[3R] <machine-3R@lanenga.net>
2021-08-06 14:42:38 +0200

    first code

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+//const gpio = require('rpi-gpio');
+
+//gpio.setup(18, gpio.DIR_OUT, err => {console.error(err);});
 
 console.log("Hello world");
+let led = 0;
+function toggle() {
+    led = (led+1)%2;
+    console.log("led:", led);
+}
+const action = setInterval(toggle, 1000);
+setTimeout(function(){clearInterval(action);}, 10000);
