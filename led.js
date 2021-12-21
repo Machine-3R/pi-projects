26b5124cdbf468bae08276e526cbd7e6b60fa52e
Machine[3R] <machine-3R@lanenga.net>
2021-12-21 14:02:19 +0100

    used setInterval instead of sleep()

--- led.js
+++ led.js
@@ -10,19 +10,17 @@ rpio.init({
  * Set the initial state to low.  The state is set prior to the pin
  * being actived, so is safe for devices which require a stable setup.
  */
-rpio.open(7, rpio.OUTPUT, rpio.LOW);
+let value = false;
+rpio.open(7, rpio.OUTPUT, value);
 
-/*
- * The sleep functions block, but rarely in these simple programs does
- * one care about that.  Use a setInterval()/setTimeout() loop instead
- * if it matters.
- */
-for (var i = 0; i < 5; i++) {
-        /* On for 1 second */
-        rpio.write(7, rpio.HIGH);
-        rpio.sleep(1);
+// toggle led every second
+let t = setInterval(function() {
+    value = !value;
+    rpio.write(7, value);
+}, 1000);
 
-        /* Off for half a second (500ms) */
-        rpio.write(7, rpio.LOW);
-        rpio.msleep(500);
-}
+// end interval after 10 seconds
+// script will end
+setTimeout(function(){
+    clearInterval(t);
+}, 10000);
