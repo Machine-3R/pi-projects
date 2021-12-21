3debc6476f4c9df20f34f26f5b5c75ce70d341a4
Machine[3R] <machine-3R@lanenga.net>
2021-12-21 17:14:14 +0100

    added interval to blink led

--- rg-led.js
+++ rg-led.js
@@ -1,11 +1,18 @@
 var gpio = require('rpi-gpio');
 
-gpio.setup(7, gpio.DIR_LOW, write);
-
-function write(err) {
+gpio.setup(7, gpio.DIR_OUT, gpio.DIR_LOW, function (err) {
     if (err) throw err;
-    gpio.write(7, true, function(err) {
-        if (err) throw err;
-        console.log('Written to pin');
-    });
-}
+
+    let value = false;
+    let t = setInterval(function () {
+        value = !value;
+        gpio.write(7, value, function (err) {
+            if (err) throw err;
+            console.log('Written to pin');
+        });
+    }, 1000);
+
+    setTimeout(function () {
+        clearInterval(t);
+    }, 10000);
+});
