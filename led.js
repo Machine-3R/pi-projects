dc93c65f10f983a31bb12a8a3be0cf62aa9620dd
Machine[3R] <machine-3R@lanenga.net>
2021-12-21 14:07:18 +0100

    added logging to console

--- led.js
+++ led.js
@@ -10,13 +10,14 @@ rpio.init({
  * Set the initial state to low.  The state is set prior to the pin
  * being actived, so is safe for devices which require a stable setup.
  */
-let value = false;
+let value = 0;
 rpio.open(7, rpio.OUTPUT, value);
 
 // toggle led every second
 let t = setInterval(function() {
-    value = !value;
-    rpio.write(7, value);
+    value++;
+    rpio.write(7, value%2);
+    console.log(value, value%2, rpio.read(7));
 }, 1000);
 
 // end interval after 10 seconds
