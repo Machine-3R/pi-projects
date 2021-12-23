5460ada806a7d58183431d5926993588537cdaaa
Machine[3R] <machine-3R@lanenga.net>
2021-12-23 17:52:19 +0100

    changed port

--- express.js
+++ express.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 server
-    .listen(80, () => {
+    .listen(8080, () => {
         console.log('Server started listening...');
     });
