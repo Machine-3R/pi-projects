a0125a4e07b52579ef1d5aed831d1db6f465a3f4
Machine[3R] <machine-3R@lanenga.net>
2021-12-26 15:02:03 +0100

    express published index.html

--- express.js
+++ express.js
@@ -4,6 +4,10 @@ const server = require('http')
     .createServer(app);
 
 app
+    .get('/*', (req, res) => {
+        console.log('requested:'.req);
+        res.sendFile(__dirname + '/public/index.html');
+    })
     .get('*', async (req, res) => {
         console.log(req.method, req.path, req.params);
         res.send('Request received ' + Math.floor(Math.random() * 1000000));
