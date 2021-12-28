d57d96bd31e3b9aee73ad53488eb78c2dd733ba7
Machine[3R] <machine-3R@lanenga.net>
2021-12-28 17:06:13 +0100

    try steppermotor

--- express.js
+++ express.js
@@ -21,26 +21,44 @@ io
         socket.emit('welcome', 'Welcome on this server.');
     });
 
-gpio.setup(7, gpio.DIR_OUT);
 { // pin 7 led blink
-    let value = false;
-    t = setInterval(function () {
-        gpio.read(7, function (err, data) {
-            console.log('7:', value);
-            value = !data;
-        });
-        gpio.write(7, value);
-        gpio.emit('change', 7, value);
-    }, 1000);
+    // gpio.setup(7, gpio.DIR_OUT);
+    // let value = false;
+    // t1 = setInterval(function () {
+    //     gpio.read(7, function (err, data) {
+    //         console.log('7:', value);
+    //         value = !data;
+    //     });
+    //     gpio.write(7, value);
+    //     gpio.emit('change', 7, value);
+    // }, 1000);
 
-    setTimeout(function () {
-        clearInterval(t);
-        console.log('cleared interval.');
-        gpio.reset();
-        console.log('gpio reset.')
-    }, 10000);
+    // setTimeout(function () {
+    //     clearInterval(t1);
+    //     console.log('cleared interval.');
+    //     gpio.reset();
+    //     console.log('gpio reset.')
+    // }, 10000);
 }
-
+{ // steppermotor
+    gpio.setup(18, gpio.DIR_OUT);
+    gpio.setup(22, gpio.DIR_OUT);
+    gpio.setup(24, gpio.DIR_OUT);
+    gpio.setup(26, gpio.DIR_OUT);
+    let step = [0, 0, 0, 1]
+    console.log(step);
+    t2 = setInterval(function () {
+        step.push(step.shift());
+        console.log(step);
+        gpio.write(18, step[0]);
+        gpio.write(22, step[1]);
+        gpio.write(24, step[2]);
+        gpio.write(26, step[3]);
+    }, 100);
+    setTimeout(function () {
+        clearInterval(t2);
+    }, 3000)
+}
 //gpio.setup(32, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 gpio
