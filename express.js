5dec1dda24a23caf49a18e958317ce85deed944b
Machine[3R] <machine-3R@lanenga.net>
2021-12-28 15:15:21 +0100

    try to read pin

--- express.js
+++ express.js
@@ -23,10 +23,13 @@ io
 
 gpio.setup(7, gpio.DIR_OUT, (err) => {
     err && console.log('ERR:', err);
+    console.log(7, gpio.read(7, function(err, value){
+        console.log('read: 7:', err, value);
+    }));
 
     let value = false;
     t = setInterval(function () {
-        value = !gpio.read(7,(err, value)=>{return value;});
+        value = !value;
         console.log('7:', value);
         gpio.write(7, !value);
     }, 1000);
@@ -38,9 +41,10 @@ gpio.setup(7, gpio.DIR_OUT, (err) => {
 });
 gpio.setup(32, gpio.DIR_IN, gpio.EDGE_BOTH);
 
+
 gpio
     .on('change', (channel, value) => {
-        console.log('channel', channel, 'changed to ', value);
+        console.log('change:', 'channel', channel, 'changed to ', value);
         io.emit('gpio.change', { channel, value });
     });
 
