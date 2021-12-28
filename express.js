8c598f142eaf012706efd60c74087f2e99466a27
Machine[3R] <machine-3R@lanenga.net>
2021-12-28 14:09:37 +0100

    using gpio instead of gpiop

--- express.js
+++ express.js
@@ -21,35 +21,29 @@ io
         socket.emit('welcome', 'Welcome on this server.');
     });
 
-gpiop.setup(7, gpio.DIR_OUT)
-    .then(() => {
-        let value = false;
-        t = setInterval(function () {
-            value = !value;
-            console.log('7:', value);
-            gpio.write(7, !value, function (err, value) {
-                console.log('ERR_WRITE:', err, 'value:', value);
-            });
-        }, 1000);
+gpio.setup(7, gpio.DIR_OUT, (err) => {
+    if (err) { throw err; };
 
-        setTimeout(function () {
-            clearInterval(t);
-            gpio.reset()
-        }, 20000)
-        
-    }, (reason)=>{
-        console.log(reason);
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    let value = false;
+    t = setInterval(function () {
+        value = !value;
+        console.log('7:', value);
+        gpio.write(7, !value);
+    }, 1000);
+
+    setTimeout(function () {
+        clearInterval(t);
+        gpio.reset()
+    }, 20000)
+
+});
 //gpio.setup(32, gpio.DIR_IN, gpio.EDGE_BOTH);
 gpio
     .on('change', (channel, value) => {
         console.log('channel', channel, 'changed to ', value);
         io.emit('gpio.change', { channel, value });
     });
-console.log('gpio events set');
+
 server
     .listen(8080, () => {
         console.log('Server started listening...');
