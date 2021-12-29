34c3377f721a57de932723f3ad476e7951035504
Machine[3R] <machine-3R@lanenga.net>
2021-12-29 17:39:18 +0100

    added blink led  to watch.js

--- watch.js
+++ watch.js
@@ -50,3 +50,24 @@ FSWatcher
         // }
     })
     ;
+
+var gpio = require('rpi-gpio');
+
+gpio.setup(7, gpio.DIR_LOW, function (err) {
+    if (err) throw err;
+
+    let value = false;
+    let t = setInterval(function () {
+        value = !value;
+        gpio.write(7, value, function (err) {
+            if (err) throw err;
+            console.log('Written to pin 7:', value);
+        });
+    }, 1000);
+
+    setTimeout(function () {
+        clearInterval(t);
+        gpio.reset();
+        process.kill(process.pid, 'SIGTERM');
+    }, 10000);
+});
