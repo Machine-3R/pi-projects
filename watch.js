d058a772d4024b953e3b7a7b8eae76f4e633c085
Machine[3R] <machine-3R@lanenga.net>
2021-12-30 01:45:01 +0100

    diabled led blinking

--- watch.js
+++ watch.js
@@ -55,27 +55,27 @@ FSWatcher
     ;
 
 // delayed led blinking
-setTimeout(function () {
-    var gpio = require('rpi-gpio');
+// setTimeout(function () {
+//     var gpio = require('rpi-gpio');
 
-    gpio.setup(7, gpio.DIR_LOW, function (err) {
-        if (err) throw err;
+//     gpio.setup(7, gpio.DIR_LOW, function (err) {
+//         if (err) throw err;
 
-        let value = false;
-        let t = setInterval(function () {
-            value = !value;
-            gpio.write(7, value, function (err) {
-                if (err) throw err;
-                console.log('Written to pin 7:', value);
-            });
-        }, 1000);
+//         let value = false;
+//         let t = setInterval(function () {
+//             value = !value;
+//             gpio.write(7, value, function (err) {
+//                 if (err) throw err;
+//                 console.log('Written to pin 7:', value);
+//             });
+//         }, 1000);
 
-        setTimeout(function () {
-            clearInterval(t);
-            // gpio.destroy(() => {
-            // });
-            process.kill(process.pid,'SIGTERM');
-        }, 10500);
-    });
+//         setTimeout(function () {
+//             clearInterval(t);
+//             // gpio.destroy(() => {
+//             // });
+//             process.kill(process.pid,'SIGTERM');
+//         }, 10500);
+//     });
 
-}, 10000);
+// }, 10000);
