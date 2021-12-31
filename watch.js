4bee1f2cefce09625ea0296aabd1aa53b693668a
Machine[3R] <machine-3R@lanenga.net>
2021-12-31 14:34:15 +0100

    minimized watch.js

--- watch.js
+++ watch.js
@@ -1,61 +1,28 @@
 const chokidar = require('chokidar');
 const fs = require('fs');
 
-class Pin {
-    constructor(gpio, direction, value) {
-        this.gpio = parseInt(gpio) || '';
-        this.direction = direction || 'in';
-        this.value = ''+parseInt(value) || '0';
-    }
-}
-
-let dir = '/sys/class/gpio/gpio*/*';
-let options = {
-    persistent: true, // default: true
-    ignoreInitial: false, // fire events for addDir and unlinkDir
-    followSymlinks: true,
-    events: 'add,change,unlink,addDir,unlinkDir,error',
-    ignored: [
-        /(^|[\/\\])\../, // ignore dotfiles
-        '/sys/class/gpio/gpiochip0', // GPIO controller
-        '/sys/class/gpio/gpiochip100',// GPIO controller
-        /\/sys\/class\/gpio\/gpio\d*\/device/,
-        /\/sys\/class\/gpio\/gpio\d*\/subsystem/,
-        /\/sys\/class\/gpio\/gpio\d*\/uevent/,
-        /\/sys\/class\/gpio\/gpio\d*\/active_low/,
-        /\/sys\/class\/gpio\/gpio\d*\/edge/,
-    ]
-};
-let ready = false;
-let FSWatcher = chokidar.watch(dir, options);
-FSWatcher
+let FSWatcher = chokidar
+    .watch('/sys/class/gpio/gpio*/*', {
+        persistent: true, // default: true
+        ignoreInitial: true, // fire events for addDir and unlinkDir
+        followSymlinks: true,
+        events: 'add,change,unlink,addDir,unlinkDir,error',
+        ignored: [
+            /(^|[\/\\])\../, // ignore dotfiles
+            '/sys/class/gpio/gpiochip*', // GPIO controller
+            /\/sys\/class\/gpio\/gpio\d*\/device/,
+            /\/sys\/class\/gpio\/gpio\d*\/subsystem/,
+            // /\/sys\/class\/gpio\/gpio\d*\/uevent/,
+            // /\/sys\/class\/gpio\/gpio\d*\/active_low/,
+            // /\/sys\/class\/gpio\/gpio\d*\/edge/,
+        ],
+        cwd: '/sys/class/gpio'
+    })
     .on('ready', () => {
-        console.log('ready', FSWatcher.getWatched()['/sys/class/gpio']);
-        ready = true;
+        console.log('ready', FSWatcher.getWatched());
     })
-    // .on('all', (event, path, stats) => {
-    //     console.log('all:', event, path);
-    // })
-    .on('add', (path, stats) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) throw err;
-            console.log('added file:', ready, path, '=>', data.trim());
-        });
-    })
-    .on('change', (path, stats) => {
-        fs.readFile(path, 'utf8', (err, data) => {
-            if (err) throw err;
-            console.log('changed file:', path, '=>', data.trim());
-        });
-    })
-    .on('unlink', (path) => {
-        console.log('deleted file:');
-    })
-    .on('addDir', (path, stats) => {
-        console.log('added dir:', path);
-    })
-    .on('unlinkDir', (path, stats, extra) => {
-        console.log('deleted dir:', path);
+    .on('all', (event, path, stats) => {
+        console.log('all:', event, path);
     })
     .on('error', (err) => {
         console.log('error:', err);
@@ -69,29 +36,3 @@ FSWatcher
         // }
     })
     ;
-
-// delayed led blinking
-// setTimeout(function () {
-//     var gpio = require('rpi-gpio');
-
-//     gpio.setup(7, gpio.DIR_LOW, function (err) {
-//         if (err) throw err;
-
-//         let value = false;
-//         let t = setInterval(function () {
-//             value = !value;
-//             gpio.write(7, value, function (err) {
-//                 if (err) throw err;
-//                 console.log('Written to pin 7:', value);
-//             });
-//         }, 1000);
-
-//         setTimeout(function () {
-//             clearInterval(t);
-//             // gpio.destroy(() => {
-//             // });
-//             process.kill(process.pid,'SIGTERM');
-//         }, 10500);
-//     });
-
-// }, 10000);
