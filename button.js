b6f2cfcd4756a1d2f614db77e70d7e05c959f8b2
Machine[3R] <machine-3R@lanenga.net>
2021-12-31 17:14:11 +0100

    change setup direction

--- button.js
+++ button.js
@@ -3,4 +3,4 @@ var gpio = require('rpi-gpio');
 gpio.on('change',(channel, value) =>{
     console.log('channel:', channel, 'value:', value);
 });
-gpio.setup(32, gpio.DIR_OUT, gpio.EDGE_BOTH);
+gpio.setup(32, gpio.DIR_IN, gpio.EDGE_BOTH);
