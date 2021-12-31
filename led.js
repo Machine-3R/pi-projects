0eaf526fd5cb2e6b1f8de5fa5e27077137e21d4b
Machine[3R] <machine-3R@lanenga.net>
2021-12-31 16:28:25 +0100

    removed code out of callback

--- led.js
+++ led.js
@@ -2,21 +2,21 @@ var gpio = require('rpi-gpio');
 
 gpio.setup(7, gpio.DIR_LOW, function (err) {
     if (err) throw err;
-
-    let value = false;
-    let t = setInterval(function () {
-        value = !value;
-        gpio.write(7, value, function (err) {
-            if (err) throw err;
-            console.log('GPIO4 (pin 7):', value);
-        });
-    }, 1000);
-
-    setTimeout(function () {
-        clearInterval(t);
-        gpio.destroy((err)=>{
-            console.log(err);
-        });
-        process.kill(process.pid, 'SIGTERM');
-    }, 10000);
 });
+
+let value = false;
+let t = setInterval(function () {
+    value = !value;
+    gpio.write(7, value, function (err) {
+        if (err) throw err;
+        console.log('GPIO4 (pin 7):', value);
+    });
+}, 1000);
+
+setTimeout(function () {
+    clearInterval(t);
+    gpio.destroy((err) => {
+        console.log(err);
+    });
+    process.kill(process.pid, 'SIGTERM');
+}, 10000);
