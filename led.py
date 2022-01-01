708b6f4dba098585bc77e67d420b98e9f2b66cf6
Machine[3R] <machine-3R@lanenga.net>
2022-01-01 12:44:38 +0100

    refactored script

--- led.py
+++ led.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-import RPi.GPIO as GPIO # Import Raspberry Pi GPIO library
-from time import sleep # Import the sleep function from the time module
+import RPi.GPIO as GPIO  # Import Raspberry Pi GPIO library
+from time import sleep  # Import the sleep function from the time module
 
-GPIO.setwarnings(False) # Ignore warning for now
-GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
-GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW) # Set pin 7 to be an output pin and set initial value to low (off)
+GPIO.setwarnings(False)  # Ignore warning for now
+GPIO.setmode(GPIO.BOARD)  # Use physical pin numbering
 
+# Set pin 7 to be an output pin and set initial value to low (off)
+GPIO.setup(7, GPIO.OUT, initial=GPIO.LOW)
+
+value = False
 while True:                     # Run forever
-    GPIO.output(7, GPIO.HIGH)   # Turn on
     sleep(1)                    # Sleep for 1 second
-    GPIO.output(7, GPIO.LOW)    # Turn off
-    sleep(1)                    # Sleep for 1 second
+    GPIO.output(7, value)   # Turn on
+    value = value and False
