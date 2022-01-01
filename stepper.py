59820fe78c194263d34ae3b4b7a21cdf119bb2f9
Machine[3R] <machine-3R@lanenga.net>
2022-01-01 15:03:14 +0100

    created class

--- stepper.py
+++ stepper.py
@@ -1,45 +1,61 @@
 import time
 import sys
-from gpiozero import OutputDevice as stepper
-IN1 = stepper(25)
-IN2 = stepper(8)
-IN3 = stepper(7)
-IN4 = stepper(1)
-stepPins = [IN1,IN2,IN3,IN4] # Motor GPIO pins</p><p>
-stepDir = -1        # Set to 1 for clockwise
-                           # Set to -1 for anti-clockwise
-mode = 1            # mode = 1: Low Speed ==> Higher Power
-                           # mode = 0: High Speed ==> Lower Power
-if mode:              # Low Speed ==> High Power
-  seq = [[1,0,0,1], # Define step sequence as shown in manufacturers datasheet
-             [1,0,0,0], 
-             [1,1,0,0],
-             [0,1,0,0],
-             [0,1,1,0],
-             [0,0,1,0],
-             [0,0,1,1],
-             [0,0,0,1]]
-else:                    # High Speed ==> Low Power 
-  seq = [[1,0,0,0], # Define step sequence as shown in manufacturers datasheet
-             [0,1,0,0],
-             [0,0,1,0],
-             [0,0,0,1]]
-stepCount = len(seq)
-if len(sys.argv)>1: # Read wait time from command line
-  waitTime = int(sys.argv[1])/float(1000)
-else:
-  waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
-stepCounter = 0
-while True:                          # Start main loop
-  for pin in range(0,4):
-    xPin=stepPins[pin]          # Get GPIO
-    if seq[stepCounter][pin]!=0:
-      xPin.on()
-    else:
-      xPin.off()
-  stepCounter += stepDir
-  if (stepCounter >= stepCount):
-    stepCounter = 0
-  if (stepCounter < 0):
-    stepCounter = stepCount+stepDir
-  time.sleep(waitTime)     # Wait before moving on
+from gpiozero import OutputDevice
+
+class Stepper:
+    def __init__(self, IN1, IN2, IN3, IN4):
+        self.stepPins = [IN1, IN2, IN3, IN4]    # Motor GPIO pins</p><p>
+        self.stepDir = -1                       # Set to 1 for clockwise
+                                                # Set to -1 for anti-clockwise
+        self.mode = 1                           # mode = 1: Low Speed ==> Higher Power
+                                                # mode = 0: High Speed ==> Lower Power
+
+        if self.mode:                           # Low Speed ==> High Power
+            self.seq = [                        # Define step sequence as shown in manufacturers datasheet
+                [1, 0, 0, 1],
+                [1, 0, 0, 0],
+                [1, 1, 0, 0],
+                [0, 1, 0, 0],
+                [0, 1, 1, 0],
+                [0, 0, 1, 0],
+                [0, 0, 1, 1],
+                [0, 0, 0, 1]
+            ]
+        else:                                   # High Speed ==> Low Power
+            seq = [                             # Define step sequence as shown in manufacturers datasheet
+                [1, 0, 0, 0],
+                [0, 1, 0, 0],
+                [0, 0, 1, 0],
+                [0, 0, 0, 1]
+            ]
+        self.stepCount = len(seq)
+        if len(sys.argv) > 1:  # Read wait time from command line
+            self.waitTime = int(sys.argv[1])/float(1000)
+        else:
+            self.waitTime = 0.004    # 2 miliseconds was the maximun speed got on my tests
+        self.stepCounter = 0
+
+        def run(self):
+            while True:                          # Start main loop
+                for pin in range(0, 4):
+                    xPin = self.stepPins[pin]          # Get GPIO
+                    if self.seq[self.stepCounter][pin] != 0:
+                        xPin.on()
+                    else:
+                        xPin.off()
+
+                self.stepCounter += self.stepDir
+                if (self.stepCounter >= self.stepCount):
+                    self.stepCounter = 0
+                if (self.stepCounter < 0):
+                    self.stepCounter = self.stepCount+self.stepDir
+                time.sleep(self.waitTime)     # Wait before moving on
+
+
+IN1 = OutputDevice(25)
+IN2 = OutputDevice(8)
+IN3 = OutputDevice(7)
+IN4 = OutputDevice(1)
+
+stepper = Stepper(IN1, IN2, IN3, IN4)
+stepper.run()
