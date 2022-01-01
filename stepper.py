a2228ab24a3801512171e2cc6e7d2d60d146c700
Machine[3R] <machine-3R@lanenga.net>
2022-01-01 16:23:23 +0100

    added stop method

--- stepper.py
+++ stepper.py
@@ -3,8 +3,6 @@ import sys
 from gpiozero import OutputDevice
 import threading
 from functools import wraps
-
-
 def delay(delay=0.):
     """
     Decorator delaying the execution of a function for a while.
@@ -17,39 +15,35 @@ def delay(delay=0.):
         return delayed
     return wrap
 
-
 class Timer():
     toClearTimer = False
-
     def setTimeout(self, fn, time):
         isInvokationCancelled = False
-
         @delay(time)
         def some_fn():
-            if (self.toClearTimer is False):
-                fn()
-            else:
-                print('Invokation is cleared!')
+                if (self.toClearTimer is False):
+                        fn()
+                else:
+                    print('Invokation is cleared!')        
         some_fn()
         return isInvokationCancelled
-
     def setClearTimer(self):
         self.toClearTimer = True
 # end class Timer
 
-
 class Stepper:
     def __init__(self, pin1, pin2, pin3, pin4):
+        self.isStarted = False
         IN1 = OutputDevice(pin1)
         IN2 = OutputDevice(pin2)
         IN3 = OutputDevice(pin3)
         IN4 = OutputDevice(pin4)
         self.stepPins = [IN1, IN2, IN3, IN4]    # Motor GPIO pins
         self.stepDir = -1                       # Set to 1 for clockwise
-        # Set to -1 for anti-clockwise
-        # mode = 1: Low Speed ==> Higher Power
+                                                # Set to -1 for anti-clockwise
+                                                # mode = 1: Low Speed ==> Higher Power
         self.mode(1)
-        # mode = 0: High Speed ==> Lower Power
+                                                # mode = 0: High Speed ==> Lower Power
 
         if self._mode:                          # High Power
             self.seq = [                        # Define step sequence as shown in manufacturers datasheet
@@ -72,18 +66,18 @@ class Stepper:
         self.stepCount = len(self.seq)
         if len(sys.argv) > 1:                   # Read wait time from command line
             self.waitTime = int(sys.argv[1])/float(1000)
-        else:
-            # 2 miliseconds was the maximun speed got on my tests
-            self.waitTime = 0.004
+        else:                          
+            self.waitTime = 0.004               # 2 miliseconds was the maximun speed got on my tests
         self.stepCounter = 0
 
     def mode(self, mode):
         self._mode = mode % 2
         print('Stepper:mode', self._mode)
 
-    def run(self):
-        print('Stepper:', 'mode', self._mode)
-        def step():
+    def start(self):
+        print('Stepper:','mode',self._mode)
+        self.isStarted = True
+        while self.isStarted:                             # Start main loop
             for pin in range(0, 4):
                 xPin = self.stepPins[pin]       # Get GPIO
                 if self.seq[self.stepCounter][pin] != 0:
@@ -92,21 +86,22 @@ class Stepper:
                     xPin.off()
 
             self.stepCounter += self.stepDir
-            self.stepCounter = self.stepCounter%self.stepCount
-        timer = Timer()
-        timer.setTimeout(step, self.waitTime)
+            if (self.stepCounter >= self.stepCount):
+                self.stepCounter = 0
+            if (self.stepCounter < 0):
+                self.stepCounter = self.stepCount+self.stepDir
+            time.sleep(self.waitTime)           # Wait before moving on
+
+    def stop(self):
+        self.isStarted = False
 # end class Stepper
 
-
+        
 stepper = Stepper(25, 8, 7, 1)
 timer = Timer()
-
-
 def changeMode():
-    print('change mode')
-    stepper.mode(1)
-
-
+    print('stop')
+    stepper.stop(1)
 timer.setTimeout(changeMode, 3)
 
 stepper.mode(0)
